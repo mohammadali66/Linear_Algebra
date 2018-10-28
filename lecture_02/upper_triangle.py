93cca84b02ee1c3226c618e45fe635d71985a360
Mohammad Ali <mohammadali.khandan@gmail.com>
2018-10-28 20:40:03 +0330

    Update upper_triangle.py

--- lecture_02/upper_triangle.py
+++ lecture_02/upper_triangle.py
@@ -9,8 +9,14 @@ import numpy as np
 A = np.matrix([[1,2,1], [3,8,1], [0,4,1]])
 b = np.array([2,12,2])
 
+# level 1: substract 2 * row1 from row2:
 E_2_1 = np.matrix([[1,0,0], [-3,1,0], [0,0,1]])
-
-print('A * E_1_1:')
+print('level1: E_1_1 * A:')
 u_2_1 = np.matmul(E_2_1, A)
 print(u_2_1)
+
+# level 2: substract 2 * row2 from row3:
+E_3_2 = np.matrix([[1,0,0], [0,1,0], [0,-2,1]])
+print('level2: E_3_2 * u_2_1')
+u_3_2 = np.matmul(E_3_2, u_2_1)
+print(u_3_2)
